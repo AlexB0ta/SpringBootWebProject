c2ad70a364b99c7c85e0979ae384c200f8a297c6
AlexB0ota <alexbota950@yahoo.com>
2023-12-16 17:42:25 +0200

    first

---
+++ src/test/java/com/example/proiectbd/ProiectBdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proiectbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectBdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
